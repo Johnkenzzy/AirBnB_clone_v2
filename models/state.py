4241a02282844cc2fe5c53463a7c6f944194e920
johnkenzzy <johnkenumeh@gmail.com>
2025-01-05 18:54:22 +0100

    Table relationship fix

--- models/state.py
+++ models/state.py
@@ -9,13 +9,5 @@ class State(BaseModel, Base):
     """ State class definition """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state')
-
-    @property
-    def cities(self):
-        """Return the list of City instances as state_id equal State's id."""
-        from models import storage
-        from models.city import City
-
-        all_cities = storage.all(City).values()  # Fetch all City objects
-        return [city for city in all_cities if city.state_id == self.id]
+    cities = relationship(
+            'City', backref='state', cascade='all, delete-orphan')
