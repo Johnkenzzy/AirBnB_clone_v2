6d78f4c83042be13f5a1db2eaec2965ea54af90d
johnkenzzy <johnkenumeh@gmail.com>
2025-01-06 05:39:04 +0100

    Fix review model

--- models/review.py
+++ models/review.py
@@ -11,7 +11,5 @@ class Review(BaseModel, Base):
     place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     text = Column(String(1024), nullable=False)
-    place = relationship(
-            'Place', back_populates='reviews', cascade='all, delete-orphan')
-    user = relationship(
-            'User', back_populates='reviews', cascade='all, delete-orphan')
+    place = relationship('Place', back_populates='reviews')
+    user = relationship('User', back_populates='reviews')
